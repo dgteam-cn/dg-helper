b95cef005ae89ac3de9c6df68e93de76cd527159
2681137811 <349118212@qq.com>
2020-03-07 10:17:42 +0800

    1

---
+++ plugins/time.js
@@ -0,0 +1,40 @@
+module.exports = {
+    Time(data,format="yyyy-MM-dd hh:mm:ss"){
+        return this.UnTimestamp(data,format)
+    },
+    Timestamp(date){
+        let time = date ? new Date(date) : new Date()
+        return Date.parse(time) / 1000
+    },
+    UnTimestamp(time=new Date(),format="yyyy-MM-dd hh:mm:ss") {
+        if(typeof time === 'string' || typeof time === 'number'){
+            if(time % 1 === 0 && time < 10000000000){
+                time = new Date(time*1000)
+            }else{
+                time = new Date(time)
+            }
+        }
+        if(time===null || time === '0000-00-00 00:00:00' || time==''){
+            time = new Date(0)
+        }
+        let date = {
+           "M+" : time.getMonth() + 1,
+           "d+" : time.getDate(),
+           "h+" : time.getHours(),
+           "m+" : time.getMinutes(),
+           "s+" : time.getSeconds(),
+           "q+" : Math.floor((time.getMonth() + 3) / 3),
+           "S+" : time.getMilliseconds()
+        };
+        if (/(y+)/i.test(format)) {
+            format = format.replace(RegExp.$1, (time.getFullYear() + '').substr(4 - RegExp.$1.length));
+        }
+        for (let k in date) {
+           if (new RegExp("(" + k + ")").test(format)) {
+                format = format.replace(RegExp.$1, RegExp.$1.length == 1
+                    ? date[k] : ("00" + date[k]).substr(("" + date[k]).length));
+           }
+        }
+        return format
+    },
+}
