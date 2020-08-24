0db4d4fe43053c997e2f013961313068c14b57ba
2681137811 <349118212@qq.com>
2020-08-24 19:49:56 +0800

    1

--- src/hash.ts
+++ src/hash.ts
@@ -8,15 +8,15 @@ const Md5 = (str: string): string => Md5JS(str)
 //         [key: string]: any,
 //     }
 // }
-const Base64 = (str: string | number, mode: string = 'encodeURI'): string => {
-    if (mode === 'encodeURI') return Base64JS.encodeURI(str.toString())
+const Base64 = (str: string | number, mode: string = 'encodeURL'): string => {
+    if (mode === 'encodeURL') return Base64JS.encodeURL(str.toString())
     else if (mode === 'encode') return Base64JS.encode(str.toString())
     else if (mode === 'decode') return Base64JS.encode(str.toString())
     else throw Error('mode 格式错误')
 }
-const Base64EncodeURI = (str: string | number): string => Base64JS.encodeURI(str.toString())
+const Base64EncodeURL = (str: string | number): string => Base64JS.encodeURL(str.toString())
 const Base64Encode = (str: string | number): string => Base64JS.encode(str.toString())
 const Base64Decode = (str: string | number): string => Base64JS.decode(str.toString())
 export = {
-    Md5, Base64, Base64Encode, Base64EncodeURI, Base64Decode
+    Md5, Base64, Base64Encode, Base64EncodeURL, Base64Decode
 }
